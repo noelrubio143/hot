d40ed1e14d0e3599c341974ddf02e4004013c275
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-27 08:38:18 +0800

    Update m-trojan.sh

--- menu/m-trojan.sh
+++ menu/m-trojan.sh
@@ -3,7 +3,7 @@ MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
 clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m      •SENSI TROJAN MENU •          \E[0m"
+echo -e "\E[0;100;33m      •AMBERVPN TROJAN MENU •          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[36m•1\e[0m] Create Account Trojan "
